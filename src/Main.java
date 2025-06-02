795b1175c48a277fb224f0913371a924f812dc1e
Jonas <jonassales510@gmail.com>
2025-06-02 09:00:28 -0300

    Alteração nos valores, dobrando a quantidade de neuronios e aumentando o learning rate para 2.8

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,11 @@ public class Main {
                 {1,0,0}, {1,0,1}, {1,1,0}, {1,1,1}
         };
 
-        final int epocas = 3000;
+        final int epocas = 2500;
 
-        NeuralNetwork rede = new NeuralNetwork(1, entradas.length, 3, 2.53); // 1 entrada, 25 ocultos, 3 saídas
+        //Bons valores entradas * 2 e learning rate 2.53
+        //2.8 ótimos resultados
+        NeuralNetwork rede = new NeuralNetwork(1, entradas.length*2, 3, 2.8); // 1 entrada, 25 ocultos, 3 saídas
         Treinamento treinamento = new Treinamento();
 
         treinamento.treinar(rede, entradas, saidas, epocas);
